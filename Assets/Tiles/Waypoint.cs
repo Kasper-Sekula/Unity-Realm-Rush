86bbef3be17efd49c6c4a002604e1b3e5141a140
Kasper Sekuła <kasper.sekula@gmail.com>
2021-11-19 15:07:36 +0100

    Added mouse input.

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool isPlacable = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnMouseDown(){
+        if (isPlacable){
+            Debug.Log(transform.name);
+        }
     }
 }
